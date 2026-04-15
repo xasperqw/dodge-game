8617d41d282e156b8303bf6994452d019f99a12e
xas <xasperqw@gmail.com>
2026-04-15 19:05:17 +0300

    Добавил заголовочный файл для основного класса игры

---
+++ game.h
@@ -0,0 +1,69 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <vector>
+
+struct Unit
+{
+    sf::CircleShape shape;
+    sf::Vector2f target;
+};
+
+struct Bullet
+{
+    sf::CircleShape shape;
+    sf::Vector2f velocity;
+};
+
+class Game
+{
+public:
+    Game();
+    void run();
+
+private:
+    const float unitRadius = 30.f;
+    const float bulletRadius = 12.f;
+    const float unitSpeed = 220.f;
+    const float bulletSpeed = 250.f;
+
+    sf::RenderWindow window;
+    sf::RectangleShape field;
+    std::vector<Unit> units;
+    std::vector<Bullet> bullets;
+    sf::CircleShape enemy;
+
+    int activeUnit = 0;
+    int score = 0;
+    bool gameOver = false;
+
+    sf::Clock deltaClock;
+    sf::Clock shootClock;
+
+    sf::Font font;
+    bool fontLoaded = false;
+    sf::Text titleText;
+    sf::Text scoreText;
+    sf::Text gameOverText;
+
+    sf::Texture unitTexture;
+    sf::Texture enemyTexture;
+    sf::Texture bulletTexture;
+    bool hasUnitTexture = false;
+    bool hasEnemyTexture = false;
+    bool hasBulletTexture = false;
+
+    void processEvents();
+    void update(float dt);
+    void render();
+    void resetGame();
+    void createUnits();
+    void shoot();
+    void moveUnits(float dt);
+    void moveBullets(float dt);
+    void updateText();
+    void centerText(sf::Text& text, sf::Vector2f pos);
+
+    float length(sf::Vector2f v);
+    sf::Vector2f normalize(sf::Vector2f v);
+    float distance(sf::Vector2f a, sf::Vector2f b);
+};
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "Game.h"
+
+int main()
+{
+
+    return 0;
+}
