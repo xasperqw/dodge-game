3979ca0c79ce9ded7e6bf41fbada911b5044ad27
xas <xasperqw@gmail.com>
2026-04-16 03:04:56 +0300

    Главный цикл и обработка ввода

--- game.cpp
+++ game.cpp
@@ -34,3 +34,64 @@ Game::Game()
 
     resetGame();
 }
+
+void Game::run()
+{
+    while (window.isOpen())
+    {
+        float dt = deltaClock.restart().asSeconds();
+        processEvents();
+        update(dt);
+        render();
+    }
+}
+
+void Game::processEvents()
+{
+    while (const std::optional event = window.pollEvent())
+    {
+        if (event->is<sf::Event::Closed>())
+        {
+            window.close();
+        }
+        else if (const auto* key = event->getIf<sf::Event::KeyPressed>())
+        {
+            if (key->scancode == sf::Keyboard::Scancode::Num1)
+                activeUnit = 0;
+            else if (key->scancode == sf::Keyboard::Scancode::Num2)
+                activeUnit = 1;
+            else if (key->scancode == sf::Keyboard::Scancode::Num3)
+                activeUnit = 2;
+            else if (key->scancode == sf::Keyboard::Scancode::R && gameOver)
+                resetGame();
+        }
+        else if (const auto* mouse = event->getIf<sf::Event::MouseButtonPressed>())
+        {
+            if (!gameOver && mouse->button == sf::Mouse::Button::Right)
+            {
+                sf::Vector2f click(
+                    static_cast<float>(mouse->position.x),
+                    static_cast<float>(mouse->position.y)
+                );
+
+                if (field.getGlobalBounds().contains(click))
+                    units[activeUnit].target = click;
+            }
+        }
+    }
+}
+
+void Game::update(float dt)
+{
+    if (!gameOver)
+    {
+        moveUnits(dt);
+
+        if (shootClock.getElapsedTime().asSeconds() >= 1.f)
+        {
+            shoot();
+            shootClock.restart();
+        }
+
+        moveBullets(dt);
+    }
